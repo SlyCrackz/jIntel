b27046b77ef10adcb3a44d065c46da188583b1b8
slycrackz <rorymulligan@protonmail.ch>
2023-12-08 14:42:54 -0500

    project updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    java
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.crackz"
@@ -13,4 +14,14 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.11")
     implementation("com.formdev:flatlaf:3.2.5")
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("jIntel")
+    archiveClassifier.set("fat")
+    archiveVersion.set(version.toString())
+
+    manifest {
+        attributes["Main-Class"] = "jIntel" // Replace with your main class
+    }
 }
